f8b28d68ceb5265d6055093bf9d2b65ae278e123
DenisKhrV <DenisKhr@yandex.ru>
2023-11-06 10:17:50 +1000

    Сделано ДЗ

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        StringListImpl stringList = new StringListImpl(5);
+        stringList.equals(null);
 
     }
 }
